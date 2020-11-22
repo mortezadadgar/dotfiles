7aa66fbbe62e48e46ade4f38c64507aa19fbc094
mortezadadgar <mortezadadgar97@gmail.com>
2020-11-22 16:04:32 +0000

    dotfiles: update dotfiles manager script
    https://github.com/flipflop133/Dotfiles_manager/commit/830df02d422107c3bf2bbeb1625a22db2d4b9320
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- dotfiles_manager.sh
+++ dotfiles_manager.sh
@@ -24,18 +24,18 @@ backup_utility(){
     echo -e "\e[96mBackup directory:\e[0m"
     echo "$dot"
 
-    # dot files directories
-    directories=()
-    input="dotfiles_list.txt"
-    while IFS= read -r line
-    do
-        IFS=''
-        directories+=($line)
-        IFS=$DefaultIFS
-    done < "$input"
-
     # backup files
     backup(){
+        # dot files directories
+        directories=()
+        input="dotfiles_list.txt"
+        while IFS= read -r line
+        do
+            IFS=''
+            directories+=($line)
+            IFS=$DefaultIFS
+        done < "$input"
+
         # Set IFS to '' so spaces are not ignored
         IFS=''
         # copy dotfiles to backup directory
@@ -68,6 +68,44 @@ backup_utility(){
         echo ''
     }
 
+    backup_system(){
+        # backup system files
+        read -r -p "Backup system files? [y/N]" response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then    
+            # get system files directories
+            system_directories=()
+            input="system_files.txt"
+            while IFS= read -r line
+            do
+                IFS=''
+                system_directories+=($line)
+                IFS=$DefaultIFS
+            done < "$input"           
+
+            # Set IFS to '' so spaces are not ignored
+            IFS=''
+            # create .root directory if it doesn't exist 
+            if [ ! -d ".root" ]
+            then
+                mkdir -p ".root"
+            fi
+            # copy system file to backup directory
+            echo -e "\e[96mBackuping the following system files:\e[0m"
+            for dir in ${system_directories[@]}; do
+                # copy the system file 
+                echo "$dir"
+                cp --parents -r -f "$dir" $dot/.root/
+            done
+            echo -e "\e[96mSystem files backup done!\e[0m"
+            # Reset IFS to its default value
+            IFS=$DefaultIFS
+            echo ''
+        else
+            echo -e "\e[96mSkipping system files backup.\e[0m"
+        fi
+        echo ''
+    }
 
     # push files to the user repo
     user_repo=$(git config --get remote.origin.url)
@@ -91,6 +129,7 @@ backup_utility(){
     # execute the functions
     backup
     backup_app
+    backup_system
     git
     echo -e "\e[96mBackup completed!\e[0m"
 }
@@ -98,19 +137,19 @@ backup_utility(){
 # Restore utility
 restore_utility(){
     clear
-
-    # dot files directories
-    directories=()
-    input="dotfiles_list.txt"
-    while IFS= read -r line
-    do
-        IFS=''
-        directories+=($line)
-        IFS=$DefaultIFS
-    done < "$input"
-
+    
     # restore files
     restore(){
+        # dot files directories
+        directories=()
+        input="dotfiles_list.txt"
+        while IFS= read -r line
+        do
+            IFS=''
+            directories+=($line)
+            IFS=$DefaultIFS
+        done < "$input"
+
         # Set IFS to '' so spaces are not ignored
         IFS=''
         # copy dotfiles to their directories
@@ -131,9 +170,64 @@ restore_utility(){
         IFS=$DefaultIFS
         echo ''
     }
+    
+    restore_system(){
+        # restore system files
+        read -r -p "Restore system files? [y/N]" response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then    
+            # get system files directories
+            system_directories=()
+            input="system_files.txt"
+            while IFS= read -r line
+            do
+                IFS=''
+                system_directories+=($line)
+                IFS=$DefaultIFS
+            done < "$input"
+            if [ $(id -u) -eq 0 ]
+            then
+                # Set IFS to '' so spaces are not ignored
+                IFS=''
+                # copy system files to their directories
+                echo -e "\e[96mRestoring the following system files:\e[0m"
+                for dir in ${system_directories[@]}; do
+                    echo "$dir"
+                    if [ -d "$dot/.root$dir/." ]; then
+                        # copy directory
+                        cp -r -f "$dot/.root$dir/." "$dir"
+                    else
+                        #copy file
+                        cp -r -f "$dot/.root$dir" "$dir"
+                    fi
+                done
+                echo -e "\e[96mSystem files restore done!\e[0m"
+                # Reset IFS to its default value
+                IFS=$DefaultIFS
+                echo ''    
+            else
+                echo -e "\e[31mPlease run script as root.\e[0m"
+                exit
+            fi
+        else
+            echo -e "\e[96mSkipping system files restore.\e[0m"
+        fi
+        echo ''
+    }
 
     # execute the functions
-    restore
+    echo -e "\e[96m1)Restore dotfiles\e[0m"
+    echo -e "\e[96m2)Restore system files\e[0m"
+    read -r -p "Enter a selection:" response
+    if [ "$response" == "1" ]; then
+        restore 
+    elif [ "$response" == "2" ]; then
+        restore_system
+    else
+        clear
+        echo -e "\e[31mPlease enter a valid value\e[0m"
+        backup
+    fi
 }
 
 # MAIN
