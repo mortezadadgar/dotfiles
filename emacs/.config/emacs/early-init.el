c1cf63a0b2e769af98bad7681222a7c348e76d67
mortezadadgar <mortezadadgar97@gmail.com>
2025-07-02 13:02:29 +0330

    remove emacs directory

--- emacs/.config/emacs/early-init.el
+++
@@ -1,7 +0,0 @@
-;; annoyance suppression
-(setq byte-compile-warnings '(not obsolete))
-(setq warning-suppress-log-types '((comp) (bytecomp)))
-(setq native-comp-async-report-warnings-errors 'silent)
-
-;; Increase gc threshold
-(setq gc-cons-threshold (* 10 1024 1024))
