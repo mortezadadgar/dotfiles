dcb8d1bedb4a9e4d8cfa55c6048799f710b8cc15
mortezadadgar <mortezadadgar97@gmail.com>
2025-07-05 23:32:15 +0330

    feat(nvim): replace vim-surround by mini-surround

---
+++ nvim/.config/nvim/lua/plugins/mini-surround.lua
@@ -0,0 +1,9 @@
+return {
+	"echasnovski/mini.surround",
+	version = "*",
+	opts = {
+		n_lines = 1000,
+		respect_selection_type = true,
+		search_method = "cover_or_next",
+	},
+}
--- nvim/.config/nvim/lua/plugins/vim-surround.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"tpope/vim-surround",
-	dependencies = {
-		"tpope/vim-repeat",
-	},
-}
